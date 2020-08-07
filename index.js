c30d8e1c47911c4e64ecaff28c0c15c56a8298f9
Abuchi Kingsley <abuchikingsley76@gmail.com>
2020-08-07 16:31:53 +0100

    changed port

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ app.use((err, req, res, next) => {
 
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 8080, () => {
+server.listen(process.env.PORT || 5000, () => {
     console.log('Server is running on port 8080');
 });
 
